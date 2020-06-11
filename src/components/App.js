0a8acfc95cb723d20bf687ff683c433faf53acde
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2020-06-11 06:51:12 +0545

    initial ttrials ..

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+import Navbar from './Navbar'
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <h1>Hello It works new worlds!!</h1>
+            </div>
+        )
+    }
+}
+
+export default App
